97e18066d83d191b06bab8085421cf0f6c37a0cc
moonryc <rycmoon@gmail.com>
2020-10-07 20:06:24 -0500

    Updated to working Version

--- src/pages/SqrtCalc/SqrtCalc.js
+++ src/pages/SqrtCalc/SqrtCalc.js
@@ -14,7 +14,8 @@ export default class SqrtCalc extends Component {
             input2: 0,
             input3: 0,
             outputOne: 0,
-            outputTwo: 0
+            outputTwo: 0,
+            test: 0
         }
     }
 
@@ -36,6 +37,12 @@ export default class SqrtCalc extends Component {
         })
     }
 
+    handelInputTest = (e) => {
+        this.setState({
+            test: e.target.value
+        })
+    }
+
     myFunction = () => {
         this.setState({
             text: "fuck this is alot" + this.state.x
@@ -47,43 +54,39 @@ export default class SqrtCalc extends Component {
         let MyAnswerTwo = 0;
         let input1 = parseFloat(this.state.input1);
         let input2 = parseFloat(this.state.input2);
-        let input3= parseFloat(this.state.input3);
-        let partTwo=input2*input2-4*input1*input3;
-            MyAnswerOne= -input2/(2*input1)
-            MyAnswerOne= MyAnswerOne.toFixed(2);
-            MyAnswerTwo= MyAnswerOne;
-            if(partTwo<0){
-                let imaginary = Math.sqrt(-partTwo)/(2*input1);
-                imaginary= imaginary.toFixed(2);
-                MyAnswerOne+= "+"+ imaginary+"i";
-                MyAnswerTwo+= "-"+ imaginary+"i";
-            }else if(partTwo>0){
-                MyAnswerOne+= Math.sqrt(partTwo)/(2*input1);
-                MyAnswerTwo+= Math.sqrt(partTwo)/(2*input1);
-            }
+        let input3 = parseFloat(this.state.input3);
+        let partTwo = input2 * input2 - 4 * input1 * input3;
+        MyAnswerOne = -input2 / (2 * input1)
+        MyAnswerOne = MyAnswerOne.toFixed(2);
+        MyAnswerTwo = MyAnswerOne;
+        if (partTwo < 0) {
+            let imaginary = Math.sqrt(-partTwo) / (2 * input1);
+            imaginary = imaginary.toFixed(2);
+            MyAnswerOne += "+" + imaginary + "i";
+            MyAnswerTwo += "-" + imaginary + "i";
+        } else if (partTwo > 0) {
+            MyAnswerOne += Math.sqrt(partTwo) / (2 * input1);
+            MyAnswerTwo += Math.sqrt(partTwo) / (2 * input1);
+        }
 
         this.setState({
             outputOne: MyAnswerOne,
             outputTwo: MyAnswerTwo
         })
     }
-
-
     render() {
-        let x = 5;
         return (
             <div>
-
+                <AutosizeInput name="form-field-name" value={this.state.test ? this.state.test : "\u00a0\u00a0\u00a0\u00a0"} onChange={this.handelInputTest} />
                 <button onClick={this.myFunction}> {this.state.text}</button> <br /><br />
                 <TextField id="filled-basic" label="Filled" variant="filled" value={this.state.input1} onChange={this.handelInputOneChange} /> <br /><br />
                 <TextField id="filled-basic" label="Filled" variant="filled" value={this.state.input2} onChange={this.handelInputTwoChange} /> <br /><br />
                 <TextField id="filled-basic" label="Filled" variant="filled" value={this.state.input3} onChange={this.handelInputThreeChange} /> <br /><br />
                 <button onClick={this.sqrtCalc}>Click here to Calculate your Polynomial Roots!</button>
-                <h3> Your equation is <i>f(x)</i> ={this.state.input1}X<sup>2</sup> +{this.state.input2}X+{this.state.input3}</h3>
-                <h1>Polynomial Root X<sub>1</sub> = {this.state.outputOne}</h1> 
+                <h3> Your equation is <i>f(x)</i> = {this.state.input1 ? this.state.input1 : ""} X<sup>2</sup> + {this.state.input2 ? this.state.input2 : ""} X {this.state.input3 ? " + " + this.state.input3 : " + 0"}</h3>
+                <h1>Polynomial Root X<sub>1</sub> = {this.state.outputOne}</h1>
                 <h1>Polynomial Root X<sub>2</sub> = {this.state.outputTwo}</h1> <br />
-
-            </div>
+            </div >
         )
     }
 }
